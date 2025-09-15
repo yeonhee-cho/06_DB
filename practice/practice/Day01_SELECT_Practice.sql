a9b8bcca15e2f7b2337366d1dba85694d3f63a8d
yeonhee-cho <yhcho.proj@gmail.com>
2025-09-15 18:09:58 +0900

    select 문제 풀이 중

--- practice/practice/Day01_SELECT_Practice.sql
+++ practice/practice/Day01_SELECT_Practice.sql
@@ -1,43 +1,51 @@
 -- 문제 1
 -- chun_university 데이터베이스의 STUDENT 테이블에서 
 -- 모든 학생의 학번(STUDENT_NO), 이름(STUDENT_NAME), 주소(STUDENT_ADDRESS)를 조회하시오.
-
+SELECT student_no, student_name, student_address
+FROM student;
 
 -- 문제 2
 -- PROFESSOR 테이블의 모든 데이터를 조회하시오.
-
+SELECT * FROM professor;
 
 -- 문제 3
 -- DEPARTMENT 테이블에서 학과번호(DEPARTMENT_NO)와 학과명(DEPARTMENT_NAME)을 조회하시오.
-
+SELECT department_no, department_name 
+FROM department;
 
 -- 문제 4
 -- STUDENT 테이블에서 모든 학생의 이름, 입학일, 입학일로부터 현재까지의 일수를 조회하시오.
 -- (컬럼명은 각각 '학생이름', '입학일', '재학일수'로 별칭 지정)
-
+SELECT student_name AS 학생이름, entrance_date AS 입학일, datediff(curdate(), entrance_date) AS 재학일수
+FROM student;
 
 -- 문제 5
 -- 현재 시간과 어제, 내일을 조회하시오.
 -- (컬럼명은 각각 '현재시간', '어제', '내일'로 별칭 지정)
+SELECT NOW() AS 현재시간, NOW() + interval 1 DAY AS 내일, now() - interval 1 DAY AS 어제;
 
-
--- 문제 6
+-- 문제 6 // TODO
 -- STUDENT 테이블에서 학번과 이름을 연결하여 하나의 컬럼으로 조회하시오.
 -- (컬럼명은 '학번_이름'으로 별칭 지정)
-
+SELECT student_no, student_name AS 학번_이름
+FROM student;
 
 -- 문제 7
 -- STUDENT 테이블에서 존재하는 학과번호의 종류만 중복 없이 조회하시오.
-
+SELECT distinct department_no
+FROM student;
 
 -- 문제 8
 -- GRADE 테이블에서 중복 없이 존재하는 학기번호(TERM_NO) 종류를 조회하시오.
-
+SELECT distinct term_no
+FROM grade;
 
 -- 문제 9
 -- STUDENT 테이블에서 휴학여부(ABSENCE_YN)가 'Y'인 학생의 
 -- 학번, 이름, 학과번호를 조회하시오.
-
+SELECT student_no, student_name, department_no
+FROM student
+WHERE absence_yn = "Y";
 
 -- 문제 10
 -- DEPARTMENT 테이블에서 정원(CAPACITY)이 25명 이상인 학과의 
